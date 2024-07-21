3b03250d015de700d519579352393577f82b0259
amateur-de <danialcs606@gmail.com>
2024-07-21 18:55:59 +0500

    Added basic User Interface to rock-paper-scissors

--- app.js
+++ app.js
@@ -14,83 +14,101 @@ getComputerChoice = ()=> {
     }
    
 }
-
+/*
 getHumanChoice = ()=> {
     let choice = prompt('Enter your choice:')
     return choice
     
-}
+}*/
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRound = 1;
 
 
 
-playGame = ()=> {
 
     playRound = (humanChoice , computerChoice)=> {
+        let result = document.querySelector('.result')
+        let round = document.querySelector('.round')
+        let humanScoreSpan = document.querySelector('.humanscore')
+        let computerScoreSpan = document.querySelector('.computerscore')
         humanChoice = humanChoice.toLowerCase()
         if(humanChoice === computerChoice )
         {
-            console.log(`It's a tie`)
+            result.textContent = `It's a tie.`
         }
         else if(humanChoice === 'rock' && computerChoice === 'scissors')
         {
-            console.log(`You win!Rock beats Scissors`)
+            result.textContent = `You win!Rock beats Scissors.`
             ++humanScore
         }
         else if(humanChoice === 'paper' && computerChoice === 'rock')
         {
-            console.log(`You win!Paper beats Rock`)
+            result.textContent = `You win!Paper beats Rock.`
             ++humanScore
         }
         else if(humanChoice === 'scissors' && computerChoice === 'paper')
         {
-            console.log(`You win!Scissors beat Paper`)
+            result.textContent = `You win!Scissors beat Paper.`
             ++humanScore
         }
         else if(computerChoice === 'rock' && humanChoice === 'scissors')
         {
-            console.log(`You lose!Rock beats Scissors`)
+            result.textContent = `You lose!Rock beats Scissors.`
             ++computerScore
         }
         else if(computerChoice === 'paper' && humanChoice === 'rock')
         {
-            console.log(`You lose!Paper beats Rock`)
+            result.textContent = `You lose!Paper beats Rock.`
             ++computerScore
         }
         else if(computerChoice === 'scissors' && humanChoice === 'paper')
         {
-            console.log(`You lose!Scissors beat Paper`)
+            result.textContent = `You lose!Scissors beat Paper.`
             ++computerScore
         }
-    
-    }
-    let humanScore = 0;
-    let computerScore = 0;
-    for(let i =0;i < 5;i++)
-    {
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection)
-        console.log(`Round ${i} of 5 Finished!Scores are as follows:1`)
-        console.log('Your score:', humanScore)
-        console.log('Computer score:', computerScore)
+        round.textContent = `Round ${currentRound} of 5 Finished!Scores are as follows:1`
+        humanScoreSpan.textContent =  humanScore
+        computerScoreSpan.textContent =  computerScore
+        if(currentRound === 5)
+        {
+               if(humanScore > computerScore)
+                {
+                    result.textContent += 'You won this game.'
+                }
+                else if(computerScore > humanScore)
+                {
+                    result.textContent += 'You lost this game.'
+                }
+                else
+                {
+                    result.textContent += 'This game ends in a draw.'
+                }
+                currentRound = 1
+                humanScore = 0
+                computerScore = 0
+
+        }
+        else
+        {
+            ++currentRound
+           
+        }
         
-    }
-    if(humanScore > computerScore)
-    {
-        console.log('You won this game')
-    }
-    else if(computerScore > humanScore)
-    {
-        console.log('You lost this game')
-    }
-    else
-    {
-        console.log('This game ends in a draw')
-    }
+        
+    
 
 
 
 }
+
+let buttons = document.querySelectorAll('button')
+buttons.forEach((button)=>{
+    button.addEventListener('click' , (e)=> {
+        playRound(e.target.className , getComputerChoice())
+    })
+})
 
 
 
